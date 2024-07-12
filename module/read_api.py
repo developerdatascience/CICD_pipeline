5503034494642ff07ca3870a9a2237afd9467553
harendra263 <harendra263@gmail.com>
2024-07-13 01:00:25 +0530

    DEV environment added

--- module/read_api.py
+++ module/read_api.py
@@ -2,9 +2,9 @@ import requests
 import pandas as pd
 pd.set_option("display.max_columns", 100)
 
-#1. api_url = "https://fakestoreapi.com/docs"  for ecommerce datasets
-#2. https://reqres.in/
-#3. https://dummyapis.com/
+# 1. api_url = "https://fakestoreapi.com/docs"  for ecommerce datasets
+# 2. https://reqres.in/
+# 3. https://dummyapis.com/
 
 api = "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001"
 
