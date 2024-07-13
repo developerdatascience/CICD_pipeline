94be23b53df05e3a4c665b7b762fbe8d97522131
harendra263 <harendra263@gmail.com>
2024-07-13 11:08:43 +0530

    new function added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CICD_pipeline
 Learning to build CI CD pipeline using Github Actions
+
+uses: The uses keyword specifies that this step will run v4 of the actions/checkout action
+
+runs-on: The runs-on keyword specifies the os in which the code will run. It can ubuntu, macos, windows
--- tests/test_math_operations.py
+++ tests/test_math_operations.py
@@ -4,7 +4,7 @@ import os
 # Add the module directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'module')))
 
-from module import addition
+from module import addition, substraction
 
 
 
@@ -14,3 +14,9 @@ def test_add_two_numbers() -> None:
     assert addition.add_two_numbers(0, 0) == 0
     assert addition.add_two_numbers(-5, -5) == -10
     assert addition.add_two_numbers(100, 200) == 300
+
+
+
+def test_substract_numbers() -> None:
+    assert substraction.subtract_numbers(5, 3) == 2
+    assert substraction.subtract_numbers(10, 5) == 5
